606354450c726644ab426a6fa1bfd3a7cd51020a
lysokheng <sokheng6485@gmail.com>
2023-05-15 16:13:17 +0700

    Add Content

--- src/api/meal/services/meal.js
+++ src/api/meal/services/meal.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test service
+ * meal service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::test.test');
+module.exports = createCoreService('api::meal.meal');
---
+++ src/api/profile/controllers/profile.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ *  profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::profile.profile', ({ Strapi }) => ({
+  async createMe(ctx) {
+    try {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.badRequest(401, [ { messages: "No athorized user found!" } ]);
+      }
+      const result = await strapi.entityService.create('api::profile.profile', {
+        data: {
+          fullName: ctx.request.body.fullName,
+          phoneNumber: ctx.request.body.phoneNumber,
+          age: ctx.request.body.age,
+          imageURL: ctx.request.body.imageURL,
+          gender: ctx.request.body.gender,
+          email: user.email,
+          user: user.id
+        }
+      });
+      return result;
+    } catch (err) {
+      return ctx.badRequest(500, [ { messages: [ { id: err.message } ] } ]);
+    }
+  },
+  async getMe(ctx) {
+    try {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.badRequest(401, [ { messages: "No athorized user found!" } ]);
+      }
+      const result = await strapi.db.query('api::profile.profile').findOne({
+        where: {
+          user: {
+            id: {
+              $eq: user.id
+            }
+          }
+        },
+      });
+      return result;
+    } catch (err) {
+      return ctx.badRequest(500, [ { messages: [ { id: 'Error' } ] } ]);
+    }
+  },
+  async updateMe(ctx) {
+    try {
+      const user = ctx.state.user;
+      if (!user) {
+        return ctx.badRequest(401, [ { messages: "No authorized user found!" } ]);
+      }
+      const result = await strapi.db.query('api::profile.profile').update({
+        where: {
+          user: {
+            id: {
+              $eq: user.id
+            }
+          }
+        },
+
+        data: {
+          fullName: ctx.request.body.fullName,
+          phoneNumber: ctx.request.body.phoneNumber,
+          age: ctx.request.body.age,
+          imageURL: ctx.request.body.imageURL,
+          gender: ctx.request.body.gender,
+          email: user.email,
+          user: user.id
+        },
+      });
+      return result;
+    } catch (err) {
+      return ctx.badRequest(500, [ { messages: [ { id: 'Error' } ] } ]);
+    }
+  },
+}));
